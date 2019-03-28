ca4b91ac10e97e4054669a27dc2d35f77e74579b
ybybybybybyb <284471505@qq.com>
2019-03-28 20:49:27 +0800

    提交，需要换一台电脑工作

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var game;
+
+// 创建一个新对象实例 600px 宽 450px 高:
+game = new Phaser.Game(600, 450, Phaser.AUTO, '');
+
+// 第一个参数是我们的状态将如何被调用。
+// 第二个参数是一个对象，该对象包含需要的状态函数方法
+game.state.add('menu', menu);
+game.state.add('photoClippingArea', photoClippingArea);
+game.state.start('menu');
---
+++ js/photoClippingArea.js
@@ -0,0 +1,25 @@
+var photoClippingArea = {
+
+    preload : function() {
+        // 加载图片是必需的,这样以后我们可以创建基于他们的sprites。
+        // 第一个参数是图像的别名
+        // 第二个是文件的路径
+        game.load.image('main_return', './img/main_return.png');
+    },
+
+    create: function () {
+        // 添加一个精灵到你的游戏场景，这里的精灵将是游戏的标志
+        // 参数为： 坐标X , 坐标Y ,图像名（见上图）
+        // this.add.sprite(0, 0, 'menu');
+		this.add.text(235, 350, "这是图片编辑区", { font: "bold 16px sans-serif", fill: "#46c0f9", align: "center"});
+        this.add.button(10, 10, "main_return", this.ChangeStateToMenu(), this);
+    },
+	
+	ChangeStateToMenu: function () {
+	
+	    // 状态更改为实际的游戏
+	    this.state.start('menu');
+	
+	}
+
+};
